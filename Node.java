73dd4fdfa301dbad24e6dee8c476276753833827
Espi <ethyn.gillies@gmail.com>
2023-06-12 20:10:24 +1200

    Deque functionality and removal of Last from node

--- Node.java
+++ Node.java
@@ -1,29 +1,33 @@
 public class Node {
     
     int state;
-    Node next, last;
+    Node next;
 
     public Node(int state){
         this.state = state;
     }
 
+    /**
+     * Gets the state of this node
+     * @return The state of the node
+     */
     public int getState(){
         return state;
     }
 
+    /**
+     * Gets the next node
+     * @return The next node
+     */
     public Node getNext(){
         return next;
     }
 
-    public Node getLast(){
-        return last;
-    }
-
+    /**
+     * Sets the next node
+     * @param next The node to set next to
+     */
     public void setNext(Node next){
         this.next = next; 
-    }
-
-    public void setLast(Node last){
-        this.last = last;
     }
 }
